2f700637d6cccc2f0948c5d28853fb7179d7b6ca
byrax15 <byraxis@gmail.com>
2023-08-22 14:32:36 -0400

    confined the glfw include to reduce the macro polution

--- hello-gl.cpp
+++ hello-gl.cpp
@@ -4,7 +4,6 @@
 #include "redbook_lib/include/vgl.h"
 #include <glbinding/glbinding.h>
 #include <glbinding/gl45core/gl.h>
-#include <GLFW/glfw3.h>
 #include <print>
 
 enum VAO_IDs { Triangles, NumVAOs, };
@@ -21,7 +20,7 @@ auto init()
 	gl::glGenVertexArrays(NumVAOs, VAOs);
 	gl::glBindVertexArray(VAOs[Triangles]);
 
-	static const GLfloat vertices[NumVertices][2] = {
+	constexpr gl::GLfloat vertices[NumVertices][2] = {
 		{ -0.90f, -0.90f }, {  0.85f, -0.90f }, { -0.90f,  0.85f },  // Triangle 1
 		{  0.90f, -0.85f }, {  0.90f,  0.90f }, { -0.85f,  0.90f },  // Triangle 2
 	};
@@ -34,22 +33,22 @@ auto init()
 	{
 		{ gl::GL_VERTEX_SHADER, "triangle.vert" },
 		{ gl::GL_FRAGMENT_SHADER, "triangle.frag" },
-		{ gl::GLenum{ GL_NONE } , NULL}
+		{ gl::GL_NONE , NULL}
 	};
 
 	gl::GLuint program = LoadShaders(shaders);
 	gl::glUseProgram(program);
 
-	gl::glVertexAttribPointer(vPosition, 2, gl::GLenum{ GL_FLOAT }, GL_FALSE, 0, BUFFER_OFFSET(0));
+	gl::glVertexAttribPointer(vPosition, 2, gl::GL_FLOAT , gl::GL_FALSE, 0, BUFFER_OFFSET(0));
 	gl::glEnableVertexAttribArray(vPosition);
 }
 
 auto display()
 {
 	constexpr gl::GLfloat black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
-	gl::glClearBufferfv(gl::GLenum{ GL_COLOR }, 0, black);
+	gl::glClearBufferfv(gl::GL_COLOR, 0, black);
 	gl::glBindVertexArray(VAOs[Triangles]);
-	gl::glDrawArrays(gl::GLenum{ GL_TRIANGLES }, 0, NumVertices);
+	gl::glDrawArrays(gl::GL_TRIANGLES, 0, NumVertices);
 }
 
 #include <GLFW/glfw3.h>
