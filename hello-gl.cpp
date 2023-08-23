81f46bfe71e3cd49c4913ef22fa059a0cc23113e
byrax15 <byraxis@gmail.com>
2023-08-22 20:40:21 -0400

    completed EBO-based triangle rendering

--- hello-gl.cpp
+++ hello-gl.cpp
@@ -11,12 +11,16 @@ using namespace std;
 constexpr gl::GLfloat vertices[]{
 	.5f,
 	.5f,
+	0,
 	.5f,
 	-.5f,
+	0,
 	-.5f,
 	-.5f,
+	0,
 	-.5f,
 	.5f,
+	0,
 };
 constexpr gl::GLuint indices[]{
 	0,
@@ -40,7 +44,6 @@ struct gl_state {
 
 
 	gl_state() {
-		using namespace gl;
 		ShaderInfo shaders[]{
 			{ gl::GL_VERTEX_SHADER, "triangle.vert" },
 			{ gl::GL_FRAGMENT_SHADER, "triangle.frag" },
@@ -48,38 +51,48 @@ struct gl_state {
 		};
 		shader_prog = LoadShaders(shaders);
 
-		glGenVertexArrays(VBsize, vao);
-		glGenBuffers(VBsize, vbo);
+		using namespace gl;
+		gl::glGenVertexArrays(VBsize, vao);
+		gl::glGenBuffers(VBsize, vbo);
+		gl::glGenBuffers(VBsize, ebo);
 		// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-		glBindVertexArray(vao[0]);
+		gl::glBindVertexArray(vao[0]);
 
-		glBindBuffer(gl::GL_ARRAY_BUFFER, vbo[0]);
-		glBufferData(gl::GL_ARRAY_BUFFER, sizeof(vertices), vertices, gl::GL_STATIC_DRAW);
+		gl::glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		gl::glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
+		gl::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
+		gl::glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		gl::glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+		gl::glEnableVertexAttribArray(0);
 
 		// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		gl::glBindBuffer(GL_ARRAY_BUFFER, 0);
+#ifndef NDEBUG
+		gl::glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+#endif
 	}
 
 
 	~gl_state() {
-		using namespace gl;
-		glDeleteVertexArrays(VBsize, vao);
-		glDeleteBuffers(VBsize, vbo);
-		glDeleteProgram(shader_prog);
+		gl::glDeleteVertexArrays(VBsize, vao);
+		gl::glDeleteBuffers(VBsize, vbo);
+		gl::glDeleteBuffers(VBsize, ebo);
+		gl::glDeleteProgram(shader_prog);
 	}
 
 
 	auto display() {
-		using namespace gl;
 		gl::glClearColor(0, .2, .1, 1);
 		gl::glClear(gl::GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shader_prog);
-		glBindVertexArray(vao[0]); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		gl::glUseProgram(shader_prog);
+		// seeing as we only have a single VAO there's no need to bind it every time,
+		// but we'll do so to keep things a bit more organized
+		gl::glBindVertexArray(vao[0]);
+		// glDrawArrays(GL_TRIANGLES, 0, 6);
+		gl::glDrawElements(gl::GL_TRIANGLES, 6, gl::GL_UNSIGNED_INT, 0);
 	}
 };
 
--- shaders/triangle.vert
+++ shaders/triangle.vert
@@ -1,8 +1,8 @@
 #version 450 core
 
-layout (location = 0) in vec2 vPosition;
+layout (location = 0) in vec3 vPosition;
 
 void main()
 {
-	gl_Position = vec4(vPosition.x, vPosition.y, 0, 1);
+	gl_Position = vec4(vPosition.x, vPosition.y, vPosition.z, 1);
 }
